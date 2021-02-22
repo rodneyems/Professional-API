fa4cb7ed6eeb55dc7def3357340b5313f92db589
RENEAS <rodney.martins@live.com>
2021-02-22 01:40:59 -0300

    feat: ensure SignUpCOntroller 200 if valid data is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -27,11 +27,15 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name: name,
         email: email,
         password: password
       })
+      return {
+        statusCode: 200,
+        body: account
+      }
     } catch (error) {
       return serverError()
     }
